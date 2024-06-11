427b2d08de583cf0c7744dbfafa5ca357593ee9d
EQ11A1\Juan Ra <coscolinguada@gmail.com>
2024-06-11 09:48:12 +0200

    Errores de sintaxis corregidos (Funciona!)

--- main.js
+++ main.js
@@ -4,47 +4,49 @@ function generarArrayBidimensional() {
     return [arr, arr, arr]; //debería meter 3 veces en el array la función random
   }
 
-  if (i > 10) {
+  if (i => 10) {
 
     var array = [];
-    for (var i = Math.floor(Math.random() * 6 + 4); i < 10;)
-      // si hacemos del índice algo random, la cantidad de arrays bailará de forma aleatoria
-      //entre 4 y 10
-      break;
+    var randomNumber = Math.floor((Math.random() * 6) + 4)
+    // si hacemos del índice algo random, la cantidad de arrays bailará de forma aleatoria
+    //entre 4 y 10
+    for (i = randomNumber; i <= 10; i++) {
+      array.push(generarArray());
+    }
+
+
   }
-  //metemos un final porque si no la función no acaba nunca
-  {
-    array.push(generarArray());
-  }
+  //metemos un break al final porque si no la función no acaba nunca
+
   return array;
+}
 
 
 
-
-  function mostrarArrayBidimensional(arrayBidimensional) {
-    function mostrarArray(array) {
-      array.forEach(function (elemento) {
-        var formatedElemento = (" " + elemento).slice(-2);
-        process.stdout.write(`| ${formatedElemento} `);
-      });
-      console.log("|");
-    }
-
-    function mostrarLinea() {
-      for (var i = 0; i < 15; i++) {
-        process.stdout.write("-");
-      }
-      console.log("-");
-    }
-
-    mostrarLinea();
-    arrayBidimensional.forEach(function (array) {
-      mostrarArray(array);
-      mostrarLinea();
+function mostrarArrayBidimensional(arrayBidimensional) {
+  function mostrarArray(array) {
+    array.forEach(function (elemento) {
+      var formatedElemento = (" " + elemento).slice(-2);
+      process.stdout.write(`| ${formatedElemento} `);
     });
+    console.log("|");
   }
 
+  function mostrarLinea() {
+    for (var i = 0; i < 15; i++) {
+      process.stdout.write("-");
+    }
+    console.log("-");
+  }
 
-
-  mostrarArrayBidimensional(generarArrayBidimensional());
+  mostrarLinea();
+  arrayBidimensional.forEach(function (array) {
+    mostrarArray(array);
+    mostrarLinea();
+  });
 }
+
+
+
+mostrarArrayBidimensional(generarArrayBidimensional());
+
